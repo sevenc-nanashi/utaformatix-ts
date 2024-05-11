aeb32b8e0987ad0f02de958fb80c6cfba457130c
sevenc-nanashi <sevenc7c@sevenc7c.com>
2024-05-11 15:58:12 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,58 @@
+# utaformatix-ts / Unofficial [UtaFormatix3](https://github.com/sdercolin/utaformatix3) port for TypeScript
+[![JSR](https://jsr.io/badges/@sevenc-nanashi/utaformatix-ts)](https://jsr.io/@sevenc-nanashi/utaformatix-ts)
+
+This is an **unofficial** [UtaFormatix3](https://github.com/sdercolin/utaformatix3) port for TypeScript.
+
+> [!WARNING]
+> This project is made of dirty hacks (It even modifies webpack's output). Use at your own risk.
+
+## Usage
+
+`parse[format]` converts a binary file to [UtaFormatix Data](https://github.com/sdercolin/utaformatix-data).
+```typescript
+import * as uf from "jsr:@sevenc-nanashi/utaformatix-ts";
+
+const stdMidiData = await Deno.readFile("./standard.mid");
+const result: UfData = await uf.parseStandardMid(stdMidiData);
+
+console.log(result); // { formatVersion: 1, project: { ... } }
+```
+
+`generate[format]` converts UtaFormatix Data to a binary file.
+```typescript
+import * as uf from "jsr:@sevenc-nanashi/utaformatix-ts";
+
+const ufdata = JSON.parse(await Deno.readFile("./standard.ufdata.json"))
+
+const midResult: Uint8Array = await uf.generateStandardMid(ufdata);
+await Deno.writeFile("./standard.mid", midResult);
+
+const musicXmlResult: Uint8Array[] = await uf.generateMusicXml(ufdata);
+await Deno.writeFile("./first_track.musicxml", musicXmlResult[0]);
+await Deno.writeFile("./second_track.musicxml", musicXmlResult[1]);
+```
+
+## Installation
+
+Please follow [jsr documentation](https://jsr.io/docs/using-packages) for
+installation instructions.
+
+```bash
+# deno
+deno add @sevenc-nanashi/utaformatix-ts
+
+# npm (one of the below, depending on your package manager)
+npx jsr add @sevenc-nanashi/utaformatix-ts
+yarn dlx jsr add @sevenc-nanashi/utaformatix-ts
+pnpm dlx jsr add @sevenc-nanashi/utaformatix-ts
+bunx jsr add @sevenc-nanashi/utaformatix-ts
+```
+
+## License
+
+This project is licensed under the MIT License, see [LICENSE](/LICENSE) for more
+information.
+
+## Acknowledgements
+
+This project is based on UtaFormatix3. See [NOTICE.md](/NOTICE.md) for more information.
---
+++ typescript/mod.ts
@@ -0,0 +1,159 @@
+// @deno-types="./built.d.ts"
+import raw, {
+  type ExportResult,
+  type FeatureConfig,
+  type ImportParams,
+  type Project,
+} from "./built.mjs";
+import type { UfData } from "npm:utaformatix-data@^1.1.0";
+import JSZip from "npm:jszip@^3.10.1";
+
+const createParse =
+  (
+    parse: (file: File, params: ImportParams) => Promise<Project>,
+    ext: string,
+  ) =>
+  async (data: Uint8Array): Promise<UfData> => {
+    const result = await parse(new File([data], `data.${ext}`), {});
+    const ufData = await raw.ufdataGenerate(result, []);
+    const buffer = await raw.exportResultBlob(ufData).arrayBuffer();
+    const decoded = new TextDecoder().decode(new Uint8Array(buffer));
+    return JSON.parse(decoded);
+  };
+const createGenerate =
+  (
+    generate: (
+      project: Project,
+      features: FeatureConfig[],
+    ) => Promise<ExportResult>,
+  ) =>
+  async (data: UfData): Promise<Uint8Array> => {
+    const buffer = new TextEncoder().encode(JSON.stringify(data));
+    const project = await raw.ufdataParse(new File([buffer], "data.json"), {});
+    const result = await generate(project, []);
+    const blob = raw.exportResultBlob(result);
+    const arrayBuffer = await blob.arrayBuffer();
+    return new Uint8Array(arrayBuffer);
+  };
+const createMultiGenerate =
+  (generate: (data: UfData) => Promise<Uint8Array>) =>
+  async (data: UfData): Promise<Uint8Array[]> => {
+    const zip = await generate(data);
+    const zipReader = new JSZip();
+    await zipReader.loadAsync(zip);
+    const files = await Promise.all(
+      Object.values(zipReader.files).map(
+        async (file) =>
+          [
+            parseInt(file.name.split("_")[1]),
+            await file.async("uint8array"),
+          ] as const,
+      ),
+    );
+    files.sort(([a], [b]) => a - b);
+
+    return files.map(([, data]) => data);
+  };
+
+export type ParseFunction = (data: Uint8Array) => Promise<UfData>;
+export type GenerateFunction = (data: UfData) => Promise<Uint8Array>;
+export type MultiGenerateFunction = (data: UfData) => Promise<Uint8Array[]>;
+
+export type { UfData };
+
+// Parse functions
+
+/** Parse ccs (CeVIO's project) file */
+export const parseCcs: ParseFunction = createParse(raw.ccsParse, "ccs");
+
+/** Parse dv (DeepVocal's project) file */
+export const parseDv: ParseFunction = createParse(raw.dvParse, "dv");
+
+/** Parse MusicXML file */
+export const parseMusicXml: ParseFunction = createParse(
+  raw.musicxmlParse,
+  "musicxml",
+);
+
+/** Parse ppsf (Piapro Studio's project) file */
+export const parsePpsf: ParseFunction = createParse(raw.ppsfParse, "ppsf");
+
+/** Parse s5p (Old Synthesizer V's project?) file */
+export const parseS5p: ParseFunction = createParse(raw.s5pParse, "s5p");
+
+/** Parse Standard MIDI file */
+export const parseStandardMid: ParseFunction = createParse(
+  raw.standardmidParse,
+  "mid",
+);
+
+/** Parse svp (Synthesizer V's project) file */
+export const parseSvp: ParseFunction = createParse(raw.svpParse, "svp");
+
+/** Parse ust (UTAU's project) file */
+export const parseUst: ParseFunction = createParse(raw.ustParse, "ust");
+
+/** Parse ustx (OpenUtau's project) file */
+export const parseUstx: ParseFunction = createParse(raw.ustxParse, "ustx");
+
+/** Parse Vocaloid 1 MIDI file */
+export const parseVocaloidMid: ParseFunction = createParse(
+  raw.vocaloidmidParse,
+  "mid",
+);
+
+/** Parse vpr (VOCALOID 5's project) file */
+export const parseVpr: ParseFunction = createParse(raw.vprParse, "vpr");
+
+/** Parse vsq (VOCALOID 2's project) file */
+export const parseVsq: ParseFunction = createParse(raw.vsqParse, "vsq");
+
+/** Parse vsqx (VOCALOID 3/4's project) file */
+export const parseVsqx: ParseFunction = createParse(raw.vsqxParse, "vsqx");
+
+// Generate functions
+
+/** Generate ccs (CeVIO's project) file */
+export const generateCcs: GenerateFunction = createGenerate(raw.ccsGenerate);
+
+/** Generate dv (DeepVocal's project) file */
+export const generateDv: GenerateFunction = createGenerate(raw.dvGenerate);
+
+/** Generate s5p (Old Synthesizer V's project?) file */
+export const generateS5p: GenerateFunction = createGenerate(raw.s5pGenerate);
+
+/** Generate Standard MIDI file */
+export const generateStandardMid: GenerateFunction = createGenerate(
+  raw.standardmidGenerate,
+);
+
+/** Generate svp (Synthesizer V's project) file */
+export const generateSvp: GenerateFunction = createGenerate(raw.svpGenerate);
+
+/** Generate ustx (OpenUtau's project) file */
+export const generateUstx: GenerateFunction = createGenerate(raw.ustxGenerate);
+
+/** Generate Vocaloid 1 MIDI file */
+export const generateVocaloidMid: GenerateFunction = createGenerate(
+  raw.vocaloidmidGenerate,
+);
+
+/** Generate vpr (VOCALOID 5's project) file */
+export const generateVpr: GenerateFunction = createGenerate(raw.vprGenerate);
+
+/** Generate vsq (VOCALOID 2's project) file */
+export const generateVsq: GenerateFunction = createGenerate(raw.vsqGenerate);
+
+/** Generate vsqx (VOCALOID 3/4's project) file */
+export const generateVsqx: GenerateFunction = createGenerate(raw.vsqxGenerate);
+
+// Multi generate functions
+
+/** Generate ust (UTAU's project) file. Returns an array of ust files, separated by tracks */
+export const generateUst: MultiGenerateFunction = createMultiGenerate(
+  createGenerate(raw.ustGenerate),
+);
+
+/** Generate MusicXML file. Returns an array of MusicXML files, separated by tracks */
+export const generateMusicXml: MultiGenerateFunction =
+  createMultiGenerate(createGenerate(raw.musicxmlGenerate));
