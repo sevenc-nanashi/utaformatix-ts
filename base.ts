e86c8a6f6c2e915bb5546bc0d47a04c97580690b
sevenc-nanashi <sevenc7c@sevenc7c.com>
2024-06-02 16:41:53 +0900

    Code: Document JapaneseLyricsType

--- base.ts
+++ base.ts
@@ -340,7 +340,14 @@ export const analyzeJapaneseLyricsType = (data: UfData): JapaneseLyricsType => {
   return type.name as JapaneseLyricsType;
 };
 
-/** Type of Japanese lyrics */
+/**
+ * Type of Japanese lyrics.
+ * - `Unknown`: Unknown type.
+ * - `KanaCv`: Hiragana/Katakana CV.（"あ", "い", "う"）
+ * - `KanaVcv`: Hiragana/Katakana VCV.（"- あ", "a い", "i う"）
+ * - `RomajiCv`: Romaji CV.（"a", "i", "u"）
+ * - `RomajiVcv`: Romaji VCV.（"- a", "a i", "i u"）
+ */
 export type JapaneseLyricsType =
   | "Unknown"
   | "KanaCv"
