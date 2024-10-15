fc12ce0c944db39dee4d4ddeb6bbf3bbc27171c4
sevenc-nanashi <sevenc7c@sevenc7c.com>
2024-10-15 20:42:21 +0900

    Fix: Add expandGlob

--- tasks/deps.ts
+++ tasks/deps.ts
@@ -4,6 +4,7 @@ export { $ } from "jsr:@david/dax@0.41.0";
 export { build, emptyDir } from "jsr:@deno/dnt@^0.41.1";
 export { SEPARATOR } from "jsr:@std/path@^0.221.0";
 export { build as esbuild, type Plugin } from "npm:esbuild@^0.21.2";
+export { expandGlob } from "jsr:@std/fs@^0.224.0";
 export {
   default as licenseChecker,
   type ModuleInfos,
